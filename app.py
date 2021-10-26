be5dd232cc2a0b3cfe772c69ce5f7e5790f49182
Victor Hamel <victor.hamel@thinkcraftsolutions.com>
2021-10-26 22:04:29 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import pdfkit
+from pyvirtualdisplay import Display
+
+app = Flask(__name__)
+
+@app.route("/pdf", methods=["POST"])
+def generate_pdf():
+    #Handle request coming in
+    if request.method == "POST":
+        dataJSON = request.get_json()
+
+    #Configuration of the binary path
+    config = pdfkit.configuration(wkhtmltopdf=bytes('/usr/bin/wkhtmltopdf', 'utf-8'))
+
+    with Display():
+      pdf_file = pdfkit.from_string(dataJSON['html'], False, css=dataJSON['css'],configuration=config, options=dataJSON['options'])
+    
+    return(pdf_file)
+
